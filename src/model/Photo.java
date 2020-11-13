10173f47527c8aa1db433846158ec7df13e60cba
Feiyu Zheng <fz114@rutgers.edu>
2020-11-12 19:52:20 -0500

    renamed the class to Photo

--- src/model/Photo.java
+++ src/model/Photo.java
@@ -2,7 +2,7 @@ package model;
 
 import java.io.Serializable;
 
-public class photo implements Serializable {
+public class Photo implements Serializable {
 
     private static final long serialVersionUID = 8312109973432202320L;
 }
