5d68d74cf2b83a86cd57d5678d78f46b861e7c28
Feiyu Zheng <fz114@rutgers.edu>
2020-11-12 21:42:58 -0500

    updated attributes, constructor and some operations

--- src/model/Photo.java
+++ src/model/Photo.java
@@ -1,8 +1,44 @@
 package model;
 
-import java.io.Serializable;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
-public class Photo implements Serializable {
+public class Photo{
+    String address;
+    Date date;
+    ArrayList<Tag> tagList;
 
-    private static final long serialVersionUID = 8312109973432202320L;
+    private static class RepeatedTagException extends RuntimeException {
+        public RepeatedTagException(String s) {
+            super(s);
+        }
+    }
+
+    public Photo(String address) throws FileNotFoundException {
+        File file = new File(address);
+        if(file.exists()){
+            this.address = file.getAbsolutePath();
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(file.lastModified());
+            this.date = cal.getTime();
+            this.tagList = new ArrayList<>();
+//            System.out.println(file.getAbsolutePath());
+        }
+        else{
+            throw new FileNotFoundException("The file \"" + address + "\" doesn't exist.");
+        }
+    }
+
+    public void addTag(String tagName, String tagValue){
+        Tag newTag = new Tag(tagName, tagValue);
+        if(this.tagList.contains(newTag)){
+            throw new RepeatedTagException("The tag (" + tagName + "," + tagValue + ") already exists.");
+        }
+        else{
+            tagList.add(newTag);
+        }
+    }
 }
--- src/model/Tag.java
+++ src/model/Tag.java
@@ -1,5 +1,27 @@
 package model;
 
+import java.util.Objects;
+
 public class Tag {
     String tagName;
+    String tagValue;
+
+    public Tag(String tagName, String tagValue){
+        this.tagName = tagName;
+        this.tagValue = tagValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(tagName, tag.tagName) &&
+                Objects.equals(tagValue, tag.tagValue);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tagName, tagValue);
+    }
 }
