760689fe1b561fd96bc44d217a639418b279ddd7
Feiyu Zheng <fz114@rutgers.edu>
2020-11-13 01:30:05 -0500

    created UniqueValueTag class

---
+++ src/model/UniqueValueTag.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.awt.image.MultiPixelPackedSampleModel;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class UniqueValueTag extends Tag{
+    String tagValue;
+
+    private static class RepeatedTagValueException extends RuntimeException{
+        public RepeatedTagValueException(String s){
+            super(s);
+        }
+    }
+
+    public UniqueValueTag(String tagName, String tagValue){
+        this.tagName = tagName;
+        this.tagValue = tagValue;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.tagName + ", " + this.tagValue + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if(o instanceof MultipleValueTag){
+            return ((MultipleValueTag) o).tagName.equals(this.tagName);
+        }
+        else if(getClass() != o.getClass()){
+            return false;
+        }
+        UniqueValueTag that = (UniqueValueTag) o;
+        return Objects.equals(tagValue, that.tagValue) && Objects.equals(tagName, that.tagName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tagName, tagValue);
+    }
+}
