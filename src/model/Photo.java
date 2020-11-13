9e8c55c5b58a76997fa1fce6851994ffef0e2acb
Feiyu Zheng <fz114@rutgers.edu>
2020-11-13 01:28:39 -0500

    updated several Exception type and operation of adding tag to a photo

--- src/model/Photo.java
+++ src/model/Photo.java
@@ -17,6 +17,12 @@ public class Photo{
         }
     }
 
+    private static class TagTypeUnmatchedException extends RuntimeException{
+        public TagTypeUnmatchedException(String s) {
+            super(s);
+        }
+    }
+
     public Photo(String address) throws FileNotFoundException {
         File file = new File(address);
         if(file.exists()){
@@ -32,13 +38,47 @@ public class Photo{
         }
     }
 
-    public void addTag(String tagName, String tagValue){
-        Tag newTag = new Tag(tagName, tagValue);
+    public void addUniqueTag(String tagName, String tagValue){
+        UniqueValueTag newTag = new UniqueValueTag(tagName, tagValue);
+
+        int existedTagIndex = this.tagList.indexOf(newTag);
         if(this.tagList.contains(newTag)){
-            throw new RepeatedTagException("The tag (" + tagName + "," + tagValue + ") already exists.");
+            Tag existedTag = this.tagList.get(existedTagIndex);
+            if(existedTag instanceof MultipleValueTag){
+                MultipleValueTag multipleValueTag = (MultipleValueTag) existedTag;
+                throw new TagTypeUnmatchedException("The tag " + multipleValueTag.tagName + " already exists as an MultipleValueTag while the system tries to add the tag (" + tagName + "," + tagValue + ") as an UniqueValueTag.");
+            }
+            else{
+                throw new RepeatedTagException("The tag (" + tagName + "," + tagValue + ") already exists.");
+            }
         }
         else{
-            tagList.add(newTag);
+            this.tagList.add(newTag);
+        }
+    }
+    public void addMultipleTag(String tagName, String tagValue){
+        MultipleValueTag newTag = new MultipleValueTag(tagName);
+        newTag.addTagValue(tagValue);
+
+        int existedTagIndex = this.tagList.indexOf(newTag);
+        if(this.tagList.contains(newTag)){
+            Tag existedTag = this.tagList.get(existedTagIndex);
+            if(existedTag instanceof UniqueValueTag){
+                UniqueValueTag uniqueValueTag = (UniqueValueTag) existedTag;
+                throw new TagTypeUnmatchedException("The tag (" + uniqueValueTag.tagName + "," + uniqueValueTag.tagValue + ") already exists as an UniqueValueTag while the system tries to add the tag (" + tagName + "," + tagValue + ") as an MultipleValueTag.");
+            }
+            else{
+                throw new RepeatedTagException("The tag (" + tagName + "," + tagValue + ") already exists.");
+            }
+        }
+        else{
+            if(existedTagIndex >= 0){
+                MultipleValueTag existedTag = (MultipleValueTag) this.tagList.get(existedTagIndex);
+                existedTag.addTagValue(tagValue);
+            }
+            else{
+                this.tagList.add(newTag);
+            }
         }
     }
 }
