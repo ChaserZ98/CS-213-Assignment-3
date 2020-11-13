89e00b75cb27e2f0bf0ae724c7d614c0bf1d5539
Feiyu Zheng <fz114@rutgers.edu>
2020-11-12 19:49:54 -0500

    create Account class

---
+++ src/model/Account.java
@@ -0,0 +1,13 @@
+package model;
+
+public abstract class Account{
+    public String username;
+    public String password;
+
+    public String getUsername() {
+        return this.username;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+}
