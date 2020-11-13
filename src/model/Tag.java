ce124904aa2c117269f3bddde83f28320bc90d22
Feiyu Zheng <fz114@rutgers.edu>
2020-11-13 01:29:42 -0500

    made the Tag a abstract class and also a parent class for UniqueValueTag and MultipleValueTag

--- src/model/Tag.java
+++ src/model/Tag.java
@@ -2,26 +2,8 @@ package model;
 
 import java.util.Objects;
 
-public class Tag {
-    String tagName;
-    String tagValue;
+public abstract class Tag {
+    public String tagName;
 
-    public Tag(String tagName, String tagValue){
-        this.tagName = tagName;
-        this.tagValue = tagValue;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Tag tag = (Tag) o;
-        return Objects.equals(tagName, tag.tagName) &&
-                Objects.equals(tagValue, tag.tagValue);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(tagName, tagValue);
-    }
+    public abstract boolean equals(Object o);
 }
