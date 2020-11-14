b393e33b98c653ef879b32b5c24a053d3541c315
Feiyu Zheng <fz114@rutgers.edu>
2020-11-14 01:42:44 -0500

    Added caption attribute and updated various operations

--- src/model/Photo.java
+++ src/model/Photo.java
@@ -2,12 +2,17 @@ package model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
 
 public class Photo{
     String address;
+    String caption;
     Date date;
     ArrayList<Tag> tagList;
 
@@ -27,6 +32,7 @@ public class Photo{
         File file = new File(address);
         if(file.exists()){
             this.address = file.getAbsolutePath();
+            this.caption = "";
             Calendar cal = Calendar.getInstance();
             cal.setTimeInMillis(file.lastModified());
             this.date = cal.getTime();
@@ -38,7 +44,23 @@ public class Photo{
         }
     }
 
-    public void addUniqueTag(String tagName, String tagValue){
+    public String getCaption(){
+        return this.caption;
+    }
+
+    public void setCaption(String caption){
+        this.caption = caption;
+    }
+
+    public String getDate(){
+        return getDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public String getDate(DateFormat dateFormat){
+        return dateFormat.format(this.date.getTime());
+    }
+
+    public void addUniqueValueTag(String tagName, String tagValue){
         UniqueValueTag newTag = new UniqueValueTag(tagName, tagValue);
 
         int existedTagIndex = this.tagList.indexOf(newTag);
@@ -56,11 +78,21 @@ public class Photo{
             this.tagList.add(newTag);
         }
     }
-    public void addMultipleTag(String tagName, String tagValue){
-        MultipleValueTag newTag = new MultipleValueTag(tagName);
-        newTag.addTagValue(tagValue);
 
-        int existedTagIndex = this.tagList.indexOf(newTag);
+    public void addUniqueValueTag(UniqueValueTag tag){
+        addUniqueValueTag(tag.tagName, tag.tagValue);
+    }
+
+    public void addMultipleValueTag(String tagName, String tagValue){
+        MultipleValueTag newTag = new MultipleValueTag(tagName, tagValue);
+
+        int existedTagIndex = -1;
+        for(int i = 0; i < this.tagList.size(); i++){
+            if(this.tagList.get(i).tagName.equals(tagName)){
+                existedTagIndex = i;
+                break;
+            }
+        }
         if(this.tagList.contains(newTag)){
             Tag existedTag = this.tagList.get(existedTagIndex);
             if(existedTag instanceof UniqueValueTag){
@@ -80,5 +112,83 @@ public class Photo{
                 this.tagList.add(newTag);
             }
         }
+    }
+
+    public void addMultipleValueTag(MultipleValueTag tag){
+        addMultipleValueTag(tag.tagName, tag.tagValues.get(0));
+    }
+
+    public void deleteUniqueValueTag(String tagName){
+        for(Tag tag : this.tagList){
+            if(tag.tagName.equals(tagName)){
+                this.tagList.remove(tag);
+                break;
+            }
+        }
+    }
+
+    public void deleteUniqueValueTag(UniqueValueTag tag){
+        this.tagList.remove(tag);
+    }
+
+    public void deleteMultipleValueTag(String tagName, String tagValue){
+        for(Tag tag : this.tagList){
+            if(tag.tagName.equals(tagName)){
+                MultipleValueTag multipleValueTag = (MultipleValueTag) tag;
+                int tagValueIndex = multipleValueTag.tagValues.indexOf(tagValue);
+                if(tagValueIndex >= 0){
+                    multipleValueTag.tagValues.remove(tagValueIndex);
+                    if(multipleValueTag.tagValues.size() == 0){
+                        this.tagList.remove(multipleValueTag);
+                    }
+                }
+            }
+        }
+    }
+
+    public void deleteMultipleValueTag(MultipleValueTag tag){
+        deleteMultipleValueTag(tag.tagName, tag.tagValues.get(0));
+    }
+
+    public boolean inDateRange(Date earliestDate, Date latestDate){
+        return this.date.getTime() >= earliestDate.getTime() && this.date.getTime() <= latestDate.getTime();
+    }
+
+    public boolean hasTag(Tag targetTag){
+        if(targetTag == null){
+            return false;
+        }
+        for(Tag tag : this.tagList){
+            if(tag.tagName.equals(targetTag.tagName)){
+                if(tag instanceof UniqueValueTag && tag.getClass() == targetTag.getClass()){
+                    return ((UniqueValueTag) tag).tagValue.equals(((UniqueValueTag) targetTag).tagValue);
+                }
+                else if(tag instanceof MultipleValueTag && tag.getClass() == targetTag.getClass()){
+                    return ((MultipleValueTag) tag).tagValues.contains(((MultipleValueTag) targetTag).tagValues.get(0));
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return this.address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Photo photo = (Photo) o;
+        return Objects.equals(address, photo.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(address, caption, date, tagList);
     }
 }
