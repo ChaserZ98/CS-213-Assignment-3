44e3e7275e0a4f3c401f6db028db8510ca4d7646
Feiyu Zheng <fz114@rutgers.edu>
2020-11-14 01:38:15 -0500

    updated some Exception types and operations

--- src/model/Album.java
+++ src/model/Album.java
@@ -1,15 +1,82 @@
 package model;
 
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Objects;
 
 public class Album{
     String name;
     ArrayList<Photo> photoList;
 
+    private static class PhotoExistedException extends RuntimeException{
+        public PhotoExistedException(String s) {
+            super(s);
+        }
+    }
+
+    private static class PhotoNotFoundException extends RuntimeException{
+        public PhotoNotFoundException(String s){
+            super(s);
+        }
+    }
+
     public Album(String name){
         this.name = name;
         photoList = new ArrayList<>();
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void addPhoto(Photo photo){
+        if(this.photoList.contains(photo)){
+            throw new PhotoExistedException("The photo " + photo.address + " is already in the album.");
+        }
+        else{
+            this.photoList.add(photo);
+        }
+    }
+
+    public void deletePhoto(Photo photo){
+        if(this.photoList.contains(photo)){
+            this.photoList.remove(photo);
+        }
+        else{
+            throw new PhotoNotFoundException("The photo " + photo.address + " is not in the album.");
+        }
+    }
+
+//    public ArrayList<String> getDistinctTagNames(){
+//        ArrayList<String> result = new ArrayList<>();
+//        for(Photo photo : photoList){
+//            for(Tag tag : photo.tagList){
+//                if(result.contains(tag.tagName))
+//            }
+//        }
+//    }
+
+    public ArrayList<Photo> getPhotosByDateRange(Date earliestDate, Date latestDate){
+        ArrayList<Photo> result = new ArrayList<>();
+        for(Photo photo : this.photoList){
+            if(photo.inDateRange(earliestDate, latestDate)){
+                result.add(photo);
+            }
+        }
+        return result.size() == 0 ?  null : result;
+    }
+
+    public ArrayList<Photo> getPhotosByTag(Tag tag){
+        ArrayList<Photo> result = new ArrayList<>();
+        for(Photo photo : this.photoList){
+            if(photo.hasTag(tag)){
+                result.add(photo);
+            }
+        }
+        return result.size() == 0 ? null : result;
     }
 
     @Override
