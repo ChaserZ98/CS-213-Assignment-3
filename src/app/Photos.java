08e6214c606edf7fd03260a4c61c8dde1ca10859
Feiyu Zheng <fz114@rutgers.edu>
2020-11-18 17:38:04 -0500

    Added comment

--- src/app/Photos.java
+++ src/app/Photos.java
@@ -7,15 +7,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import model.*;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.Serializable;
-import java.util.ArrayList;
 
+/**
+ * The main class of the photo project
+ * @author Feiyu Zheng
+ * @author Ying Yu
+ */
 public class Photos extends Application{
 
+    /**
+     * The start method of the whole project that loads the login page and show the primary window
+     * @param primaryStage the primary window instance
+     */
     @Override
     public void start(Stage primaryStage){
         try {
@@ -43,41 +47,11 @@ public class Photos extends Application{
         }
     }
 
+    /**
+     * The main method of the photo project
+     * @param args argument parameter
+     */
     public static void main(String[] args){
-
-//        Photo photo1 = new Photo("data/photo1.jpg");
-//        Photo photo2 = new Photo("data/photo2.jpg");
-//        Photo photo3 = new Photo("data/photo3.jpg");
-//        Photo photo4 = new Photo("data/photo4.jpg");
-//
-//        photo1.addUniqueValueTag("unique", "value");
-//        photo2.addUniqueValueTag("unique", "value");
-//        photo3.addUniqueValueTag("unique", "value");
-//        photo4.addUniqueValueTag("unique", "value");
-//
-//        photo1.addMultipleValueTag("multiple", "value1");
-//        photo1.addMultipleValueTag("multiple", "value2");
-//
-//        photo2.addMultipleValueTag("multiple", "value2");
-//        photo2.addMultipleValueTag("multiple1", "value1");
-//
-//        photo4.addMultipleValueTag("multiple", "value2");
-//        photo4.addMultipleValueTag("multiple1", "value1");
-//        photo4.addMultipleValueTag("multiple", "value");
-//
-//        User user = new User("name");
-//        Album album = new Album("name");
-//        album.addPhoto(photo1);
-//        album.addPhoto(photo2);
-//        album.addPhoto(photo3);
-//        album.addPhoto(photo4);
-//        user.getAlbumList().add(album);
-//
-//        Tag targetTag1 = new UniqueValueTag("unique", "value");
-//        Tag targetTag2 = new MultipleValueTag("multiple", "value1");
-//        System.out.println(user.searchPhotoByTag(user.getAlbumList().get(0), targetTag2));
-////        System.out.println(user.albumList.get(0).photoList.get(3).tagList);
-
         launch(args);
     }
 }
