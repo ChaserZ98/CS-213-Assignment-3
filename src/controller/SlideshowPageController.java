7bd2c14caab2156ac5b3342a988914ff792c7af5
Feiyu Zheng <fz114@rutgers.edu>
2020-11-18 10:06:51 -0500

    Added start() method

--- src/controller/SlideshowPageController.java
+++ src/controller/SlideshowPageController.java
@@ -7,6 +7,9 @@ import model.User;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import model.Album;
+import model.User;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
